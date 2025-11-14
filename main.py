d17b541104025c8a1403626dc87861b11fdc2fdc
MihaiDUmitru9 <mihai18dumitru@yahoo.com>
2025-11-14 20:35:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("C:/Unihack2025/ObDetector/venv/runs/detect/trainMacheta2/weights/best.pt")  # build a new model from scratch
+
+# Use the model
+results = model.train(data="config.yaml", epochs=150)  # train the model
+
+#train/box_loss: 1.48821,
+# train/cls_loss: 1.35898,
+# train/dfl_loss: 1.63336,
+# performance: 0.61274
