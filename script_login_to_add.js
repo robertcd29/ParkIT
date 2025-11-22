d549e133e9284fde2c38bcd7e3aab9b616628045
Robert-Denis Chira <74325080+robertcd29@users.noreply.github.com>
2025-11-22 17:12:23 +0200

    Delete script_login_to_add.js

--- script_login_to_add.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', function() {
-            const token = localStorage.getItem('adminToken') || sessionStorage.getItem('adminToken');
-            
-            if (!token) {
-                window.location.href = 'admin-login.html';
-            }
-        });
-
